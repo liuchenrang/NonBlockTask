cfbb7cea04a3d721c11b1786d408f08824bd803f
多多 <duoduo@xiaoshijie.com>
2018-09-20 16:54:07 +0800

    update

--- NLogger.php
+++ NLogger.php
@@ -1,13 +1,8 @@
 <?php
 namespace LT\Duoduo\Task;
 
-interface Logger{
-    public  function info( $var = '');
-    public  function notify( $var = '');
-  
-}
 
-class Logger implements ILogger{
+class NLogger implements ILogger{
     public  function debug( $var = '')
     {
         $info = sprintf("%s %s\r\n",date("Y-m-d H:i:s"),$var);
